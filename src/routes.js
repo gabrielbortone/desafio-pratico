b2d220b717e3f24ef1aa161beb7d0fd4b2c290f5
Gabriel Bortone <43412854+gabrielbortone@users.noreply.github.com>
2024-06-06 08:38:10 -0300

    Tentando fazer a ROTA do CSV funcionar

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Database } from './database.js'
 import { buildRoutePath } from './utils/build-route-path.js'
+import { parse } from 'csv-parse';
+import fs from 'node:fs';
+import file from 'express-fileupload';
 
 const database = new Database()
 
@@ -8,7 +11,7 @@ export const routes = [
   {
     method: 'GET',
     path: buildRoutePath('/tasks'),
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { title, description } = req.query
 
       const tasks = database.selectTasks(title, description);
@@ -19,7 +22,7 @@ export const routes = [
   {
     method: 'POST',
     path: buildRoutePath('/tasks'),
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { title, description } = req.body
 
       const task = {
@@ -37,9 +40,31 @@ export const routes = [
     }
   },
   {
+    method: 'POST',
+    path: buildRoutePath('/tasks/csv'),
+    handler: async (req, res) => {
+      try{
+        const parser = req.pipe(
+          parse()
+        );
+        let count = 1;
+        process.stdout.write('start\n');
+        for await (const record of parser) {
+          process.stdout.write(`${count++} ${record.join(',')}\n`);
+          await new Promise((resolve) => setTimeout(resolve, 100));
+        }
+      }catch(e){
+        console.log(e);
+        res.writeHead(400).end
+      }
+
+      
+    }
+  },
+  {
     method: 'PUT',
     path: buildRoutePath('/tasks/:id'),
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { id } = req.params
       const body = req.body
       
@@ -52,7 +77,7 @@ export const routes = [
   {
     method: 'DELETE',
     path: buildRoutePath('/tasks/:id'),
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { id } = req.params
 
       database.delete(id)
@@ -63,7 +88,7 @@ export const routes = [
   {
     method: 'PATCH',
     path: buildRoutePath('/tasks/:id/complete'),
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { id } = req.params
 
       database.updateMark(id)
--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 import { extractQueryParams } from './utils/extract-query-params.js'
 
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
-
+  
   await json(req, res);
 
   const route = routes.find(route => {
@@ -21,10 +22,11 @@ const server = http.createServer(async (req, res) => {
     req.params = params;
     req.query = query ? extractQueryParams(query) : {}
 
-    return route.handler(req, res);
+    return await route.handler(req, res);
   }
 
   return res.writeHead(404).end();
 })
+
 
 server.listen(3335);
